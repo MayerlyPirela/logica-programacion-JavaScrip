3e6a9ffae410b3da969182675ba1bd14d66c7b8c
Mayerly <mayerlypirela@gmail.com>
2025-01-27 10:05:26 -0300

    se actualiza con la palabra else

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+//variables
 let numeroSecreto = 4;
-let numeroUsuario = prompt ('Me indicas un número por favor');
+let numeroUsuario = prompt ('Me indicas un número entre 1 a 10 por favor');
 
 console.log(numeroUsuario);
-
+/*
+este codigo realiza la comparación
+*/
 if (numeroUsuario == numeroSecreto) {
+   //Acertamos, fue verdadera la condición
    alert('Acertaste el número');
+} else {
+   //La condición no se cumplió
+   alert ('Lo siento no acertaste el número');
 }
 
