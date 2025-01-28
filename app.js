2702cbfe34160f0cc8e56d156dd620e5d7be0c4d
Mayerly <mayerlypirela@gmail.com>
2025-01-28 07:48:23 -0300

    update de app

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ if (numeroUsuario == numeroSecreto) {
    //Acertamos, fue verdadera la condición
    alert(`Acertaste, el número es: ${numeroUsuario}`);
 } else {
+   if (numeroUsuario > numeroSecreto){
+      alert('El número secreto es menor');
+   } else{
+      alert ('El número secreto es mayor');
+   }
    //La condición no se cumplió
-   alert ('Lo siento no acertaste el número');
+   //alert ('Lo siento no acertaste el número');
 }
 
